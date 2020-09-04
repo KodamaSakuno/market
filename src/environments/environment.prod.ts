96ea2f28da0f223d082baa7ebe3bebcbc6daf406
神樹桜乃 <kodama@sakuno.moe>
2020-09-04 23:22:25 +0800

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  marketAddress: 'n1gEtoFxn9oHWByQ4C5kRJ3Rr8wjbDh1hxx',
+  tokenAddress: 'n1t14HZ7B4miUcsL2SwL8jGxHSqUQu9paW4',
 };
