ef427527e57f689dd274e24354040efd80b436fa
神樹桜乃 <kodama@sakuno.moe>
2020-11-16 03:34:38 +0800

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  marketAddress: 'n21zZGGdARErmkuL8nnpeF4epL6D4jmkMfT',
+  marketAddress: 'n217HyJ8zmPYwRC4ro24Gc55QbxzJijuPFj',
   tokenAddress: 'n1etmdwczuAUCnMMvpGasfi8kwUbb2ddvRJ',
   proxyAddress: 'n1eKHUdvxVaPaJsbthdP2pCU64ZajEGcrNX',
   explorerPrefix: 'https://explorer.nebulas.io/#/address/',
